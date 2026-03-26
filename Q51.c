fc0e42b15183d429a52f9f9db6ce8287201264ae
ShashwatTripathi998 <shashwattripathi874@gmail.com>
2026-03-26 10:00:00 +0530

    Lowest Common Ancestor in BST

---
+++ Q51.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create node
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL)
+        return createNode(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Find LCA in BST
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    if (root == NULL) return NULL;
+
+    if (n1 < root->data && n2 < root->data)
+        return findLCA(root->left, n1, n2);
+
+    if (n1 > root->data && n2 > root->data)
+        return findLCA(root->right, n1, n2);
+
+    return root; // This is the split point → LCA
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    // Input BST elements
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* lca = findLCA(root, n1, n2);
+
+    if (lca != NULL)
+        printf("%d", lca->data);
+
+    return 0;
+}
